9a65c4943736894efc2f90dc5a2891c939069f9f
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-09-12 12:39:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InicijalnoPonavljanje2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] znakovi = new char [5];
+            znakovi[] = Convert.ToString(znakovi);
+         
+            Console.WriteLine("Unesi niz znakova: ");
+            for(int i = 0;i < 5; i++)
+            { 
+                Console.WriteLine("Znak " + i);
+                znakovi[i] = Console.ReadLine();
+            }
+            Console.ReadKey();
+           
+            
+        }
+    }
+}
